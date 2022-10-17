5920fa71334b04736a5bc9754d21890e2819b515
Alton Michaux <alton.michaux@yahoo.com>
2022-10-17 07:31:41 -0400

    added ids

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -5,27 +5,32 @@ function App() {
     {
     title: "Scary Movie 1",
     director: "Director1",
-    releaseYear: 1990
+    releaseYear: 1990,
+    id: 1
     },
     {
       title: "Scary Movie 2",
       director: "Director2",
-      releaseYear: 1995
+      releaseYear: 1995,
+      id: 2
     },
     {
       title: "Scary Movie 3",
       director: "Director3",
-      releaseYear: 2000
+      releaseYear: 2000,
+      id: 3
     },
     {
       title: "Scary Movie 4",
       director: "Director4",
-      releaseYear: 2005
+      releaseYear: 2005,
+      id: 4
     },
     {
       title: "Scary Movie 5",
       director: "Director5",
-      releaseYear: 2012
+      releaseYear: 2012,
+      id: 5
     }
   ]
 
