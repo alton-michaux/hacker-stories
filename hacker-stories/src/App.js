46c388fa2d0f6efc64fd2025a03b74e77ae8cef4
Alton Michaux <alton.michaux@yahoo.com>
2022-10-17 07:30:43 -0400

    built page with dummy data

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
 
 function App() {
+  const list = [
+    {
+    title: "Scary Movie 1",
+    director: "Director1",
+    releaseYear: 1990
+    },
+    {
+      title: "Scary Movie 2",
+      director: "Director2",
+      releaseYear: 1995
+    },
+    {
+      title: "Scary Movie 3",
+      director: "Director3",
+      releaseYear: 2000
+    },
+    {
+      title: "Scary Movie 4",
+      director: "Director4",
+      releaseYear: 2005
+    },
+    {
+      title: "Scary Movie 5",
+      director: "Director5",
+      releaseYear: 2012
+    }
+  ]
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Horror Classics
+      </h1>
+
+      <label htmlFor="search">search</label>
+      <input id="search" type="text"></input>
+
+      <hr/>
+
+      <ul>
+        { list.map(function (movie) {
+          return <li key={movie.id}><b>{movie.title}</b>, released in <b>{movie.releaseYear}</b> directed by <b>{movie.director}</b></li>
+          })
+        }
+      </ul>
     </div>
   );
 }
