0ba3711b8266b66f75628f3f1b65ebf7f410b484
Alton Michaux <alton.michaux@yahoo.com>
2022-12-10 09:49:40 -0500

    introduced a reducer to handle List state, although Im not sure what it does

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import Input from "./inputComponent";
 import Items from "./items";
 import List from "./list";
@@ -13,29 +13,40 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 };
 
+const listReducer = (state, action) => {
+  if (action.type == 'SET_LIST') {
+    return action.payload
+  } else {
+    throw new Error();
+  }
+}
+
+const getAsyncList = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { list: List } }),
+      2000
+    )
+  );
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "");
 
-  const [list, setList] = useState([]);
+  const [list, dispatchList] = useReducer(listReducer, []);
 
   const [isLoading, setIsLoading] = useState(false); // loading state
 
   const [isError, setIsError] = useState(false);
 
-  const getAsyncList = () =>
-    new Promise((resolve) =>
-      setTimeout(
-        () => resolve({ data: { list: List } }),
-        2000
-      )
-    );
-
   useEffect(() => {
     setIsLoading(true);
   
     getAsyncList().then(result => {
+      dispatchList({
+        type: 'SET_LIST',
+        payload: result.data.list,
+      })
       // console.log("result", result)
-      setList(result.data.list)
       setIsLoading(false)
     }).catch(() => setIsError(true)); // mention to Roy, can't get setIsError to work
   }, []);
@@ -47,7 +58,10 @@ const App = () => {
 
   const handleRemoveItem = (item) => {
     const newItems = list.filter((entry) => item.objectID !== entry.objectID);
-    return setList(newItems);
+    dispatchList({
+      type: 'SET_LIST',
+      payload: List
+    })
   };
 
   const filteredEntries = list.filter((entry) =>
