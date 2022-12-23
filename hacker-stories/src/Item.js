7f89ec3d2d63ee45f59133b9789e47678fbcab1f
Alton Michaux <alton.michaux@yahoo.com>
2022-12-22 23:38:17 -0500

    FINALLY GOT PICTURES

--- hacker-stories/src/Item.js
+++ hacker-stories/src/Item.js
@@ -2,16 +2,33 @@ import React from "react";
 
 const Item = ({ item, onRemoveItem }) => {
 	return (
-	< li style = {{ padding: "5px" }} className="item">
-		{/* <img src={item.primaryImage.url} alt={item.primaryImage.caption.plainText}/> */}
-		<span>{item.titleText.text} </span>
-		<br/>
-		<span>
-			<button type="button" onClick={() => onRemoveItem(item)} className="delete-btn">
-				Delete
-			</button>
-		</span>
-	</li >
-)};
+		< li style={{ padding: "5px" }} className="item">
+			{item.primaryImage ?
+				<>
+					<span>
+						<img src={item.primaryImage.url} alt={item.primaryImage.caption.plainText} width="100%" height="100%" />
+					</span>
+					<span>{item.titleText.text} </span>
+					<br />
+					<span>
+						<button type="button" onClick={() => onRemoveItem(item)} className="delete-btn">
+							Delete
+						</button>
+					</span>
+
+				</> :
+				<>
+					<span>{item.titleText.text} </span>
+					<br />
+					<span>
+						<button type="button" onClick={() => onRemoveItem(item)} className="delete-btn">
+							Delete
+						</button>
+					</span>
+				</>
+			}
+		</li >
+	)
+};
 
 export default Item;
